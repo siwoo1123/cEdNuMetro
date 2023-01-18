c9e44886ce77fbb4523b097e20c0095b6954da11
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2023-01-18 20:22:11 +0900

    fc

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Logo from '../lib/images/logo.png'
+</script>
+
+<svelte:head>
+	<script src="https://cdn.tailwindcss.com"></script>
+</svelte:head>
+
+<div class="app">
+	<nav class="bg-gray-100 p-3 flex justify-start">
+		<a href="/"><img src={Logo} class="w-30 h-24" alt="logo"></a>
+		<a class="text-black p-3 m-6 text-xl" href="/introduce">소개</a>
+		<a class="text-black p-3 m-6 text-xl" href="/inform">공지사항</a>
+		<a class="text-black p-3 m-6 text-xl" href="https://discord.gg/cYC3vsxmTF" target="_blank">입사하기</a>
+		<a class="text-black p-3 m-6 text-xl" href="https://www.youtube.com/@cednumetro" target="_blank">Youtube</a>
+	</nav>
+	<main class="p-5">
+		<slot />
+	</main>
+</div>
---
+++ src/routes/introduce/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>cEdNu Metro - 메인</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>Introduce</h1>
+</section>
